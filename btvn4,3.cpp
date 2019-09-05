74d75fbd7daf79d0487da75f7fecdc05098cd4b5
Nguyễn Giang Sơn <son10c3k56@gmail.com>
2019-09-05 21:44:33 +0700

    Create btvn4,3.cpp

---
+++ btvn4,3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int x =9;
+    while(x>8){
+    	
+        	printf("nhap thu trong tuan\n");
+        	scanf("%d",&x);
+    	}
+    printf("%d la thu trong tuan\n",x);
+	return 0;
+}
+
