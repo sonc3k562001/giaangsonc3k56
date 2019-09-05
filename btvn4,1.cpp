11feff326c53fb636150408a45a34feeb2158b3d
Nguyễn Giang Sơn <son10c3k56@gmail.com>
2019-09-05 21:44:22 +0700

    Create btvn4,1.cpp

---
+++ btvn4,1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    int i=2;
+    
+    scanf("%d",&n);
+    
+    while(i<n){
+    	if(i%2==0){
+    	
+    	printf("%d la so chan be lon hon n\n",i);
+    	}
+    	i++;
+    	}
+    
+	return 0;
+}
+
