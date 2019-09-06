196de098f2a5d198f50fb3086f7b3a4a048415cd
Nguyễn Giang Sơn <son10c3k56@gmail.com>
2019-09-06 22:27:38 +0700

    Create btvn5_1.cpp

---
+++ btvn5_1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n=1,i;
+    scanf("%d",&n);
+    while(n>0){
+	       n/=10;i++;
+	     
+	}
+    
+    printf("la so co %d chu so\n",n,i);
+	return 0;
+}
+
