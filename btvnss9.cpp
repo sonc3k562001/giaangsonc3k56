4d8ed024e13acde23088d03d307734acba9261de
Nguyễn Giang Sơn <son10c3k56@gmail.com>
2019-09-18 13:11:47 +0700

    Create btvnss9.cpp

---
+++ btvnss9.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+    char str[3][4];
+    
+    char a[4];
+    printf("vui long nhap chuoi\n");
+    scanf("%s",str);
+     
+    for(int i;i<3;i++){
+    	for(int j;j<4;j++){
+    		scanf("%4d",&str[i][j]);
+    		}
+    	
+    	
+        int stircmp(str[3][4],a[4]);
+        if(ss<0 || ss>0){
+        	printf("chuoi khong thuoc ham\n");
+        }
+        else{
+        	printf("chuoi thuoc ham\n"); break;
+		}
+    }
+	return 0;
+}
+
